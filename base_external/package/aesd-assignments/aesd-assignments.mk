ce709e1e44d8deb193c6a7623587b76bada6b243
jainaneesh <jainaneesh15@gmail.com>
2025-01-18 18:34:30 -0500

    changed aesd-assignments.mk file a lil

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,11 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '71f3935a2b2c684dd2cdc411c45f1caa3fcc08cf'
+AESD_ASSIGNMENTS_VERSION = 71f3935a2b2c684dd2cdc411c45f1caa3fcc08cf
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-jainaneesh.git'
+AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-jainaneesh.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
