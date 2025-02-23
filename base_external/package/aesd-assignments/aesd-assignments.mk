da3db79dfe63b031360e0109a9906a2296d1bae6
jainaneesh <jainaneesh15@gmail.com>
2025-02-22 19:28:17 -0500

    completely nuked the error flag

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -8,7 +8,7 @@
 #$(shell read -n 1 -s -r -p "Paused for debugging. Execution shifted to aesd-assignments.mk Press any key to continue..."; echo)
 $(info *** Starting aesd-assignments package build ***)
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = d6b4c1086f350a51b4ce7441b9348b17d2759736 
+AESD_ASSIGNMENTS_VERSION = c5a9d7dc4866f8be6f0ddca5e4b93ac363c16a24 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
